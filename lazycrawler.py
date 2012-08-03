f1d8a57c98f0e013ed79c7b29d81af13846612ab
Alexander Ose <alex@alexose.com>
2012-08-03 16:48:45 -0700

    added spider

---
+++ lazycrawler.py
@@ -0,0 +1,41 @@
+from spider import *
+
+def main():
+    opts, args = parse_options()
+
+    url = args[0]
+
+    if opts.links:
+        getLinks(url)
+        raise SystemExit, 0
+
+    depth_limit = opts.depth_limit
+    confine_prefix=opts.confine
+    exclude=opts.exclude
+
+    sTime = time.time()
+
+    print >> sys.stderr,  "Crawling %s (Max Depth: %d)" % (url, depth_limit)
+    crawler = Crawler(url, depth_limit, confine_prefix, exclude)
+    crawler.crawl()
+
+    if opts.out_urls:
+        print "\n".join(crawler.urls_seen)
+
+    if opts.out_links:
+        print "\n".join([str(l) for l in crawler.links_remembered])
+
+    if opts.out_dot:
+        d = DotWriter()
+        d.asDot(crawler.links_remembered)
+
+    eTime = time.time()
+    tTime = eTime - sTime
+
+    print >> sys.stderr, "Found:    %d" % crawler.num_links
+    print >> sys.stderr, "Followed: %d" % crawler.num_followed
+    print >> sys.stderr, "Stats:    (%d/s after %0.2fs)" % (
+            int(math.ceil(float(crawler.num_links) / tTime)), tTime)
+
+if __name__ == "__main__":
+    main()
